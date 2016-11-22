0758cccc76e2d9586a4146bb26decce4c237e8f2
Matt Gold <mgold@kabbage.com>
2016-11-22 12:11:38 -0500

    fix errors when message is not provided on /send

--- services/mailgun.js
+++ services/mailgun.js
@@ -18,7 +18,7 @@ module.exports = (toAddress, matchName, subject, message) => {
         from: 'Secret Santa <secretsanta@mattgmade.me>',
         to: toAddress,
         subject: subject || 'Your "Secret Santa" match',
-        text: message.replace('{{name}}', matchName) || `Your Secret Santa match is ${matchName}!`,
+        text: typeof(message) === 'string' ? message.replace('{{name}}', matchName) : `Your Secret Santa match is ${matchName}!`,
         'o:tracking-opens': 'yes'
     }).auth(config.mailgunUsername, config.mailgunPassword);
 };
