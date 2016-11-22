5ff2c17931ec381d484ddebe317706acc5f76222
Matt Gold <mgold@kabbage.com>
2016-11-22 08:52:28 -0500

    fix /v1 options result

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -8,11 +8,6 @@ const app = express();
 
 app.options('/', allow(['OPTIONS']), (req, res, next) => {
     res.locals.data = {
-        "/": {
-            "GET": {
-                'description': 'the app'
-            }
-        },
         "/list": "Gives authenticated users CRUD operations for building a secret santa list.",
         '/match/:listId?': 'Finds matches for all people in a secret santa list.',
         '/send/:matchResultId?': 'Sends the final secret santa emails to each person in a list according to the matches in the provided match result.'
